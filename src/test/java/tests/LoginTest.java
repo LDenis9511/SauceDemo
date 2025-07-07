fe6c13eaa7df72e1603c90f7fba4eb70aa49dcff
Denis <lapat9511@gmail.com>
2025-07-07 20:08:30 +0300

    added TestNG

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,11 @@
 package tests;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -12,7 +16,7 @@ import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest{
 
-    @Test
+    @Test (enabled = false ,groups = {"checkLogin"})
     public void checkLoginWithoutPassword(){
         loginPage.open();
         loginPage.login("standard_user","");
@@ -21,7 +25,7 @@ public class LoginTest extends BaseTest{
                 "Сообщение не то ");
     }
 
-    @Test
+    @Test (enabled = false ,groups = {"checkLogin"})
     public void checkLoginWithoutUsername(){
         loginPage.open();
         loginPage.login("","secret_sauce");
@@ -30,7 +34,7 @@ public class LoginTest extends BaseTest{
                 "Сообщение не то ");
     }
 
-    @Test
+    @Test (enabled = false ,groups = {"checkLogin"})
     public void checkLoginWithNegativeValue(){
         loginPage.open();
         loginPage.login("test","test");
@@ -39,10 +43,28 @@ public class LoginTest extends BaseTest{
                 "Сообщение не то ");
     }
 
-    @Test
+    @Test (testName = "Авторизация")
     public void checkLogin(){
         loginPage.open();
         loginPage.login("standard_user","secret_sauce");
         assertTrue(productsPage.isPageOpened(),"Не авторизовались");
+    }
+
+    @DataProvider (name =  "LoginData")
+    public  Object[][] loginData(){
+        return new Object[][]{
+                {"standard_user","", "Epic sadface: Password is required"},
+                {"", "secret_sauce", "Epic sadface: Username is required"},
+                {"test", "test", "Epic sadface: Username and password do not match any user in this service"}
+        };
+    }
+
+    @Test (dataProvider = "LoginData",testName ="Авторизация с неправильными данными")
+    public void checkLoginWithNegativeValue1(String user,String password,String expectedMessage){
+        loginPage.open();
+        loginPage.login(user,password);
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                expectedMessage,
+                "Сообщение не то ");
     }
 }
